de1ec9285dd2f64babf23575a836c1e7e7f4cf1c
denefc <mehmet_7kezman@hotmail.com>
2021-10-17 23:39:51 +0300

    some color changes

--- app/src/main/java/com/cevik/newweatherapp/adapter/SecondWeatherHolder.kt
+++ app/src/main/java/com/cevik/newweatherapp/adapter/SecondWeatherHolder.kt
@@ -1,5 +1,6 @@
 package com.cevik.newweatherapp.adapter
 
+import android.annotation.SuppressLint
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -7,15 +8,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cevik.newweatherapp.R
 import com.cevik.newweatherapp.model.SecondWeatherModel
 
-class SecondWeatherHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
-    fun bind(model:SecondWeatherModel){
-        val secondDayText=itemView.findViewById<TextView>(R.id.secondDayText)
-        val seconDegreeText=itemView.findViewById<TextView>(R.id.secondDegree)
-        val secondLogo=itemView.findViewById<ImageView>(R.id.secondLogo)
+class SecondWeatherHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        secondDayText.text=model.dayTime
-        seconDegreeText.text=model.degree
+    fun bind(model: SecondWeatherModel) {
+        val secondDayText = itemView.findViewById<TextView>(R.id.secondDayText)
+        val seconDegreeText = itemView.findViewById<TextView>(R.id.secondDegree)
+        val secondLogo = itemView.findViewById<ImageView>(R.id.secondLogo)
+
+        secondDayText.text = model.dayTime
+        seconDegreeText.text = model.degree
         secondLogo.setImageResource(model.logo)
-    }
 
+
+    }
 }
--- app/src/main/java/com/cevik/newweatherapp/adapter/WeatherHolder.kt
+++ app/src/main/java/com/cevik/newweatherapp/adapter/WeatherHolder.kt
@@ -1,5 +1,6 @@
 package com.cevik.newweatherapp.adapter
 
+import android.annotation.SuppressLint
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -7,18 +8,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cevik.newweatherapp.model.WeatherModel
 import com.cevik.newweatherapp.R
 
-class WeatherHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
+class WeatherHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind(model:WeatherModel){
-        val logo=itemView.findViewById<ImageView>(R.id.logo)
-        val dayText=itemView.findViewById<TextView>(R.id.day_text)
-        val statusText=itemView.findViewById<TextView>(R.id.status_text)
-        val degreeText=itemView.findViewById<TextView>(R.id.degree_text)
+
+    fun bind(model: WeatherModel) {
+        val logo = itemView.findViewById<ImageView>(R.id.logo)
+        val dayText = itemView.findViewById<TextView>(R.id.day_text)
+        val statusText = itemView.findViewById<TextView>(R.id.status_text)
+        val degreeText = itemView.findViewById<TextView>(R.id.degree_text)
 
         logo.setImageResource(model.logo)
-        dayText.text=model.day
-        statusText.text=model.status
-        degreeText.text=model.degree
+        dayText.text = model.day
+        statusText.text = model.status
+        degreeText.text = model.degree
 
 
     }
